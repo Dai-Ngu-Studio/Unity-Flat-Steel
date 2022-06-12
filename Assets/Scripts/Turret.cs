112f9089e7eef45b24a6106c166b4ae1daaec55a
HT-Dat <hotiendat661@gmail.com>
2022-06-13 02:37:06 +0700

    Update shoot and delay between shot mechanic

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,12 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Transform> turretBarrels;
+    public GameObject bulletPrefab;
+    public float reloadDelay = 0.5f;
+
+    private bool canShoot = true;
+    private Collider2D[] tankColliders;
+    public float currentDelay = 0;
+
+    private void Awake()
+    {
+        tankColliders = GetComponentsInParent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (canShoot == false)
+        {
+            currentDelay -= Time.deltaTime;
+            if (currentDelay <= 0)
+            {
+                canShoot = true;
+            }
+        }
+    }
+
     public void Shoot()
     {
-        Debug.Log("Shooot");
+        if (canShoot)
+        {
+            canShoot = false;
+            currentDelay = reloadDelay;
+            foreach (var barrel in turretBarrels)
+            {
+                GameObject bullet = Instantiate(bulletPrefab);
+                bullet.transform.position = barrel.position;
+                bullet.transform.localRotation = barrel.rotation;
+                bullet.GetComponent<Bullet>().Initialize();
+                foreach (var collider in tankColliders)
+                {
+                    Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), collider);
+                }
+            }
+        }
     }
 }
