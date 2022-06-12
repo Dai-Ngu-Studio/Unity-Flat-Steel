5931c98450d787cf7c3a260eb2be2c94aa261e3d
HT-Dat <hotiendat661@gmail.com>
2022-06-13 02:54:43 +0700

    Self destruct only occurs when owner

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -13,10 +13,13 @@ public class Bullet : MonoBehaviour
     private Vector2 startPosition;
     private float travelledDistance = 0;
     private Rigidbody2D rb2d;
+    private PhotonView view;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        view = GetComponent<PhotonView>();
+
     }
 
     public void Initialize()
@@ -31,13 +34,21 @@ public class Bullet : MonoBehaviour
         travelledDistance = Vector2.Distance(transform.position, startPosition);
         if (travelledDistance > maxDistance)
         {
-            PhotonNetwork.Destroy(this.gameObject);
+            SelfDestruct();
         }
     }
 
+    private void SelfDestruct()
+    {
+        if (view.IsMine)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("Collied" + col.name);
-        PhotonNetwork.Destroy(this.gameObject);
+        SelfDestruct();
+
     }
 }
