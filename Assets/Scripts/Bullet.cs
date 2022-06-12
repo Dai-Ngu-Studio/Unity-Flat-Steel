39af3d1008766d263abeed6a4e03c33332577e10
HT-Dat <hotiendat661@gmail.com>
2022-06-13 02:37:21 +0700

    Script for bullet spawn and travel logic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,14 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 100f;
     public int damage = 10;
     public float maxDistance = 10;
 
     private Vector2 startPosition;
-    private float conquaredDistance = 0;
+    private float travelledDistance = 0;
     private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Initialize()
+    {
+        startPosition = transform.position;
+        rb2d.velocity = transform.up * speed;
+    }
+
+    //Update calculate distance travelled of the bullet
+    private void Update()
+    {
+        travelledDistance = Vector2.Distance(transform.position, startPosition);
+        if (travelledDistance > maxDistance)
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Collied" + col.name);
+        Object.Destroy(this.gameObject);
+    }
 }
