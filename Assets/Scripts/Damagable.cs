59c8c9f11807a534498ad9a0c8adbc4fdebd3c28
HT-Dat <hotiendat661@gmail.com>
2022-06-15 14:56:08 +0700

    Damage script

--- Assets/Scripts/Damagable.cs
+++ Assets/Scripts/Damagable.cs
@@ -1,23 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Damagable : MonoBehaviour
 {
-    public int MaxHealth;
+    public int MaxHealth = 100;
 
-    [SerializeField]
-    private int currentHealth
+    [SerializeField] private int currentHealth;
+
+    public int Health
     {
-        get
-        {
-            return currentHealth;
-        }
+        get { return currentHealth; }
         set
         {
             currentHealth = value;
-            
+            onHealthChanged?.Invoke((float)currentHealth / MaxHealth);
         }
     }
-    
+
+    public UnityEvent onDead;
+    public UnityEvent<float> onHealthChanged;
+    public UnityEvent onHit, onHeal;
+
+    private void Start()
+    {
+        Health = MaxHealth;
+    }
+
+    public void Hit(int inflictedDamage)
+    {
+        Health -= inflictedDamage;
+        if (Health <= 0)
+        {
+            onDead?.Invoke();
+        }
+        else
+        {
+            onHit?.Invoke();
+        }
+    }
 }
