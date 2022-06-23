5b14aa1d02ebda011db2b3833c6a9727d5fe9e19
HT-Dat <hotiendat661@gmail.com>
2022-06-23 21:12:57 +0700

    Assign player object to follower camera

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using Photon.Pun;
+
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
     public float minX, minY, maxX, maxY;
-    // Start is called before the first frame update
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+
     void Start()
     {
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity);
+        var playerOnServer = PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity);
+        var playerTransform = playerOnServer.transform.Find("Tank/TankTurretParent").gameObject.transform;
+
+        Camera.main.gameObject.TryGetComponent<CinemachineBrain>(out var brain);
+        if (brain == null)
+        {
+            brain = Camera.main.gameObject.AddComponent<CinemachineBrain>();
+        }
+
+        cinemachineVirtualCamera = gameObject.GetComponent<CinemachineVirtualCamera>();
+        cinemachineVirtualCamera.Follow = playerTransform;
+        cinemachineVirtualCamera.LookAt = playerTransform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
