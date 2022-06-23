110dc034ec04f193cdf79b4002808713ae1841f2
HT-Dat <hotiendat661@gmail.com>
2022-06-23 21:11:22 +0700

    Add script to shake screen

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static CameraShake Instance { get; private set; }
+    
+    public float intensity = 3;
+    public float time = 0.1f; 
+    
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeTimer;
+
+    private void Awake()
+    {
+        Instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera()
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                    cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+
+            }
+        }
+    }
+}
