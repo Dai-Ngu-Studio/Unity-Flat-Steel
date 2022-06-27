155aea22662fe5f1857bfdd858964942a8634d5a
HT-Dat <hotiendat661@gmail.com>
2022-06-27 14:22:09 +0700

    Implement function spawn at fixed position

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -7,20 +7,37 @@ using Photon.Pun;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
-    public float minX, minY, maxX, maxY;
-
-    void Start()
+    private float X, Y;
+    public Canvas ChoosePositionCanvas;
+    public Canvas HealthCanvas;
+    public void SpawnAtPosition(int position)
     {
-        Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Debug.Log(position);
+        switch (position)
+        {
+            case 1:
+                X = -13;
+                Y = 7;
+                break;
+            case 2:
+                X = 13;
+                Y = 7;
+                break;
+            case 3:
+                X = -13;
+                Y = -7;
+                break;
+            case 4:
+                X = 13;
+                Y = -7;
+                break;
+        }
+
+        ChoosePositionCanvas.gameObject.SetActive(false);
+        HealthCanvas.gameObject.SetActive(true);
+        Vector2 randomPosition = new Vector2(X,Y);
         var playerOnServer = PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity);
         var playerTransform = playerOnServer.transform.Find("Tank/TankTurretParent").gameObject.transform;
         StaticScript.Instance.CameraFollower(playerTransform);
-
-        PhotonNetwork.CurrentRoom.IsOpen = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
